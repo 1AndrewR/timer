d000681e66bd985af2a14c8bbbd4c50d2f3118e6
Andrew Roth <a.roth.80@hotmail.com>
2024-11-16 16:17:25 -0800

    timer1.js updated

--- timer1.js
+++ timer1.js
@@ -1,10 +1,13 @@
+// Take in command line arguments
 const args = process.argv.slice(2);
 
+// Check if number is positive
 const isPositiveNumber = (value) => {
   const number = Number(value);
   return !isNaN(number) && number > 0;
 };
 
+// Loop through arguments to set timer for valid numbers
 args.forEach((arg) => {
   if (isPositiveNumber(arg)) {
     setTimeout(() => {
